8c7ce3369d534ffa649ceb1d2ceb8e5e6d79b7fa
danilocrsou <danilocruzss@icloud.com>
2024-01-15 17:57:34 -0300

    Atualizacao atualizar.dart

---
+++ atualizar.dart
@@ -0,0 +1,11 @@
+/* entrar no cmd
+
+entrar na pasta aonde ta o código 
+
+C:\Users\danil\OneDrive\Área de Trabalho\njovoprojeto
+
+git commit -m "criação bla bla bla bla"
+
+git pull origin main
+
+git push
