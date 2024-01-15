cd9c1853c464d0bc12b8696ed80da200bba679cd
danilocrsou <danilocruzss@icloud.com>
2024-01-15 17:50:12 -0300

    Atualização atualizar.dart e main.dart

--- main.dart
+++ main.dart
@@ -1,10 +1,14 @@
 import 'dart:io';
 
-void main() {
+void main(){
   print('Olá, qual é o seu nome?');
 
   String name = stdin.readLineSync()!;
-  name.isEmpty
-      ? print('Nenhum nome registrado')
-      : print('Seja muito bem-vindo(a) $name');
+  name.isEmpty ? print('Nenhum nome registrado') : print('Seja muito bem-vindo(a) $name');
+
+    print('Agora que sabemos o seu nome, qual é o seu sobrenome?');
+    String sobrenome = stdin.readLineSync()!;
+
+    print('Que legal! Agora sabemos que você se chama $name $sobrenome');
+
 }
